012ea921948f0857c35f42345e3d1000a30bdc3a
Ayan Banglawala <ayanchhipa2278@gmail.com>
2025-05-01 18:06:50 +0530

    DASHBOARD

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -5,54 +5,142 @@ import {
   GroupIcon,
 } from "../../icons";
 import Badge from "../ui/badge/Badge";
+import { LuMessageSquareText } from "react-icons/lu";
 
 export default function EcommerceMetrics() {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-4 md:gap-6">
       {/* <!-- Metric Item Start --> */}
-      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+      <div className="flex flex-row items-center justify-between rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:py-5">
+        {/* Left Icon */}
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <LuMessageSquareText className="text-gray-800 size-6 dark:text-white/90" />
+        </div>
+
+        {/* Text content */}
+        <div className="ml-4 flex-1 text-end">
+          <span className="text-sm text-end text-gray-500 dark:text-gray-400">
+            Unassign Inquiries
+          </span>
+          <h4 className="mt-1 text-end font-bold text-gray-800 text-title-sm dark:text-white/90">
+            100
+          </h4>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-between rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:py-5">
+        {/* Left Icon */}
         <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
           <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
         </div>
 
-        <div className="flex items-end justify-between mt-5">
-          <div>
-            <span className="text-sm text-gray-500 dark:text-gray-400">
-              Customers
-            </span>
-            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              3,782
-            </h4>
-          </div>
-          <Badge color="success">
-            <ArrowUpIcon />
-            11.01%
-          </Badge>
+        {/* Text content */}
+        <div className="ml-4 flex-1 text-end">
+          <span className="text-sm text-end text-gray-500 dark:text-gray-400">
+            Customers
+          </span>
+          <h4 className="mt-1 text-end font-bold text-gray-800 text-title-sm dark:text-white/90">
+            3,782
+          </h4>
         </div>
       </div>
-      {/* <!-- Metric Item End --> */}
-
-      {/* <!-- Metric Item Start --> */}
-      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+      <div className="flex flex-row items-center justify-between rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:py-5">
+        {/* Left Icon */}
         <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-          <BoxIconLine className="text-gray-800 size-6 dark:text-white/90" />
+          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
         </div>
-        <div className="flex items-end justify-between mt-5">
-          <div>
-            <span className="text-sm text-gray-500 dark:text-gray-400">
-              Orders
-            </span>
-            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              5,359
-            </h4>
-          </div>
 
-          <Badge color="error">
-            <ArrowDownIcon />
-            9.05%
-          </Badge>
+        {/* Text content */}
+        <div className="ml-4 flex-1 text-end">
+          <span className="text-sm text-end text-gray-500 dark:text-gray-400">
+            Customers
+          </span>
+          <h4 className="mt-1 text-end font-bold text-gray-800 text-title-sm dark:text-white/90">
+            3,782
+          </h4>
         </div>
       </div>
+      <div className="flex flex-row items-center justify-between rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:py-5">
+        {/* Left Icon */}
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+        </div>
+
+        {/* Text content */}
+        <div className="ml-4 flex-1 text-end">
+          <span className="text-sm text-end text-gray-500 dark:text-gray-400">
+            Customers
+          </span>
+          <h4 className="mt-1 text-end font-bold text-gray-800 text-title-sm dark:text-white/90">
+            3,782
+          </h4>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-between rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:py-5">
+        {/* Left Icon */}
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+        </div>
+
+        {/* Text content */}
+        <div className="ml-4 flex-1 text-end">
+          <span className="text-sm text-end text-gray-500 dark:text-gray-400">
+            Customers
+          </span>
+          <h4 className="mt-1 text-end font-bold text-gray-800 text-title-sm dark:text-white/90">
+            3,782
+          </h4>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-between rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:py-5">
+        {/* Left Icon */}
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+        </div>
+
+        {/* Text content */}
+        <div className="ml-4 flex-1 text-end">
+          <span className="text-sm text-end text-gray-500 dark:text-gray-400">
+            Customers
+          </span>
+          <h4 className="mt-1 text-end font-bold text-gray-800 text-title-sm dark:text-white/90">
+            3,782
+          </h4>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-between rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:py-5">
+        {/* Left Icon */}
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+        </div>
+
+        {/* Text content */}
+        <div className="ml-4 flex-1 text-end">
+          <span className="text-sm text-end text-gray-500 dark:text-gray-400">
+            Customers
+          </span>
+          <h4 className="mt-1 text-end font-bold text-gray-800 text-title-sm dark:text-white/90">
+            3,782
+          </h4>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-between rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:py-5">
+        {/* Left Icon */}
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+        </div>
+
+        {/* Text content */}
+        <div className="ml-4 flex-1 text-end">
+          <span className="text-sm text-end text-gray-500 dark:text-gray-400">
+            Customers
+          </span>
+          <h4 className="mt-1 text-end font-bold text-gray-800 text-title-sm dark:text-white/90">
+            3,782
+          </h4>
+        </div>
+      </div>
+
+      
       {/* <!-- Metric Item End --> */}
     </div>
   );
--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -14,27 +14,27 @@ export default function Home() {
         description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12 space-y-6 xl:col-span-7">
+        <div className="col-span-12 space-y-6 xl:col-span-12">
           <EcommerceMetrics />
 
-          <MonthlySalesChart />
+          {/* <MonthlySalesChart /> */}
         </div>
 
-        <div className="col-span-12 xl:col-span-5">
-          <MonthlyTarget />
-        </div>
+        {/* <div className="col-span-12 xl:col-span-5"> */}
+          {/* <MonthlyTarget /> */}
+        {/* </div> */}
 
-        <div className="col-span-12">
+        {/* <div className="col-span-12">
           <StatisticsChart />
-        </div>
+        </div> */}
 
-        <div className="col-span-12 xl:col-span-5">
+        {/* <div className="col-span-12 xl:col-span-5">
           <DemographicCard />
-        </div>
+        </div> */}
 
-        <div className="col-span-12 xl:col-span-7">
+        {/* <div className="col-span-12 xl:col-span-7">
           <RecentOrders />
-        </div>
+        </div> */}
       </div>
     </>
   );
